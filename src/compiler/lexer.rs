93594688852fd19def5b0cd97db3e700a89865df
mgleason97 <m.gleason9597@gmail.com>
2023-04-05 20:47:36 -0700

    rustier lex

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -1,3 +1,5 @@
+use std::str;
+
 use super::token::{self, Token, TokenType};
 
 pub fn lex(buf: &mut [u8]) -> Vec<Token> {
@@ -12,73 +14,64 @@ pub fn lex(buf: &mut [u8]) -> Vec<Token> {
         }
 
         if c.is_ascii_punctuation() {
-            if c == '#' {
-                tokens.push(Token(TokenType::Hash));
-            } else if c == '+' {
-                tokens.push(Token(TokenType::Plus));
-            } else if c == '-' {
-                tokens.push(Token(TokenType::Minus));
-            } else if c == '*' {
-                tokens.push(Token(TokenType::Multiply));
-            } else if c == '/' {
-                tokens.push(Token(TokenType::Divide));
-            } else if c == '(' {
-                tokens.push(Token(TokenType::LParen));
-            } else if c == ')' {
-                tokens.push(Token(TokenType::RParen));
-            } else if c == '.' {
-                tokens.push(Token(TokenType::Dot));
-            } else if c == ',' {
-                tokens.push(Token(TokenType::Comma));
-            } else if c == ';' {
-                tokens.push(Token(TokenType::SemiColon));
-            } else if c == '?' {
-                tokens.push(Token(TokenType::Question));
-            } else if c == '!' {
-                tokens.push(Token(TokenType::Exclamation));
-            } else if c == '=' {
-                tokens.push(Token(TokenType::Equal));
-            } else if c == ':' {
-                if idx + 1 < buf.len() && buf[idx+1] as char == '=' {
-                    tokens.push(Token(TokenType::Assign));
+            tokens.push(match c {
+                '#' => Token(TokenType::Hash),
+                '+' => Token(TokenType::Plus),
+                '-' => Token(TokenType::Minus),
+                '*' => Token(TokenType::Multiply),
+                '/' => Token(TokenType::Divide),
+                '(' => Token(TokenType::LParen),
+                ')' => Token(TokenType::RParen),
+                '.' => Token(TokenType::Dot),
+                ',' => Token(TokenType::Comma),
+                ';' => Token(TokenType::SemiColon),
+                '?' => Token(TokenType::Question),
+                '!' => Token(TokenType::Exclamation),
+                '=' => Token(TokenType::Equal),
+                ':' if idx + 1 < buf.len() && buf[idx+1] as char == '=' => {
                     idx += 1;
-                } else {
-                    panic!("Invalid syntax");
-                }
-            } else if c == '<' {
-                if idx + 1 < buf.len() && buf[idx+1] as char == '=' {
-                    tokens.push(Token(TokenType::LessThanEqual));
+                    Token(TokenType::Assign)
+                },
+                '<' => if idx + 1 < buf.len() && buf[idx+1] as char == '=' {
                     idx += 1;
-                } else {
-                    tokens.push(Token(TokenType::LessThan));
-                }
-            } else if c == '>' {
-                if idx + 1 < buf.len() && buf[idx+1] as char == '=' {
-                    tokens.push(Token(TokenType::GreaterThanEqual));
+                    Token(TokenType::LessThanEqual)
+                }  else {
+                    Token(TokenType::LessThan)
+                },
+                '>' => if idx + 1 < buf.len() && buf[idx+1] as char == '=' {
                     idx += 1;
-                } else {
-                    tokens.push(Token(TokenType::GreaterThan));
-                }
-            }
+                    Token(TokenType::GreaterThanEqual)
+                }  else {
+                    Token(TokenType::GreaterThan)
+                },
+                _ => panic!("Invalid syntax {:?}", c)
+            });
 
             idx+=1;
         } else if c.is_ascii_digit() {
-            let mut digit_buf = Vec::<u32>::new();
-            while c.is_ascii_digit() {
-                digit_buf.push(c.to_digit(10).expect("Safe to convert ascii digit to base 10 digit"));
+            let start = idx;
+            let end = loop {
                 idx += 1;
                 c = buf[idx] as char;
-            }
-            let num = digit_buf.iter().fold(0, |acc, &x| acc * 10 + x);
+                if !c.is_ascii_digit() {
+                    break idx;
+                }
+            };
+            let slice = str::from_utf8(&buf[start..end]).expect("Invalid utf8");
+            let num = u32::from_str_radix(slice, 10).expect("Could not parse number");
             tokens.push(Token(TokenType::Number(num)));
         } else if c.is_alphabetic() || c == '_' {
-            let mut char_buf = Vec::<char>::new();
-            while c.is_alphanumeric() || c == '_' {
-                char_buf.push(c);
+            let start = idx;
+            let end = loop {
                 idx += 1;
                 c = buf[idx] as char;
-            }
-            let word = String::from_iter(char_buf.iter());
+                if !c.is_alphanumeric() && c != '_'  {
+                    break idx;
+                }
+            };
+
+            let slice = str::from_utf8(&buf[start..end]).expect("Invalid utf8");
+            let word = String::from(slice);
 
             if let Some(reserved_token) = token::parse_keyword(&word) {
                 tokens.push(Token(reserved_token));
